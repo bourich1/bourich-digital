ccdafc3e566c49b7935e9173f0f03f04ea073bd3
bourich1 <bourichanouar@gmail.com>
2026-03-23 12:18:22 +0100

    change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <Link to="/" className="flex items-center gap-2">
-              <img src="/logo-digital-white.png" alt="Bourich Digital" className="h-10 w-auto" />
+              <img src="/logo.png" alt="Bourich Digital" className="h-10 w-auto" />
             </Link>
 
             {/* Desktop Menu */}
